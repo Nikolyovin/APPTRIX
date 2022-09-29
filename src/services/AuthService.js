b8118e96ca56d2a431f48a059937e3bbe479df7c
Лёвин Николай Сергеевич <151705@grpz.su>
2022-09-29 12:46:37 +0300

    fessef

--- src/services/AuthService.js
+++
@@ -1,7 +0,0 @@
-import apiAuth from "../http/apiAuth";
-
-export default class AuthService {
-    static async login(username, password) {
-        return apiAuth.post(`/token/`, { username, password })
-    }
-}
